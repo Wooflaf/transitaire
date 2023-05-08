b36d93e9902cb1b691682f9248a1d398cdbcf42f
Olaf <menesesolaf@gmail.com>
2023-05-08 13:10:13 +0200

    Optimizado procesado de datos de tráfico; solo se cargarán en leaflet los cambios en el estado de los tramos que varíen

--- data_cleaning/trafico_clean.R
+++ data_cleaning/trafico_clean.R
@@ -15,7 +15,30 @@ labels_estado <- c("Fluido", "Denso", "Congestionado", "Cortado", "Sin datos",
                    "Sin datos (paso inferior)")
 
 accum_trafico_rodado <- read_csv("./data/accum_trafico.csv") %>% 
-  mutate(estado = factor(estado, levels = 0:9, labels = labels_estado),
-         fecha_carga = clean_date_time(fecha_carga))
+  mutate(
+    gid = as.integer(gid),
+    estado = factor(estado, levels = 0:9, labels = labels_estado),
+    fecha_carga = clean_date_time(fecha_carga))
 
-trafico <- st_as_sf(left_join(accum_trafico_rodado, tramos_trafico, by = "gid") %>% mutate(gid = as.character(gid)))
+first_datetime <- min(accum_trafico_rodado$fecha_carga)
+last_datetime <- max(accum_trafico_rodado$fecha_carga)
+
+traffic_changed <- accum_trafico_rodado %>% 
+  mutate(fecha_carga_next = fecha_carga + hours(1)) %>% 
+  left_join(accum_trafico_rodado, 
+            by = c("gid" = "gid",
+                   "fecha_carga_next" = "fecha_carga"),
+            suffix = c("", "_next"), keep = T) %>% 
+  filter(estado != estado_next, fecha_carga_next != last_datetime) %>% 
+  select(gid_next, estado_next, fecha_carga_next) %>% 
+  rename_with(~str_replace(.x, "_next", ""))
+
+traffic_first_last <- accum_trafico_rodado %>%
+  filter(fecha_carga == first_datetime | fecha_carga == last_datetime)
+
+# Calculamos los tramos que cambian de estado para no sobrecargar el renderizado del leaflet
+traffic_variation <- rbind(traffic_first_last, traffic_changed)
+
+trafico <- st_as_sf(left_join(traffic_variation, tramos_trafico, by = "gid") %>%
+                      mutate(gid = as.character(gid)) %>% 
+                      select(-denominacion, -idtramo))
