efe4363d56285975a83e8399704c21daf1a619ed
Olaf <menesesolaf@gmail.com>
2023-05-09 00:15:01 +0200

    Revertido cambio de tomar solo datos de cuando varía el estado del tramo. En la visualización no cambiaban si movías manualmente el slider hacia detrás.

--- data_cleaning/trafico_clean.R
+++ data_cleaning/trafico_clean.R
@@ -10,6 +10,9 @@ clean_date_time <- function(x){
   return(with_tz(dt, tzone = "Europe/Madrid"))
 }
 
+first_datetime <- min(accum_trafico_rodado$fecha_carga)
+last_datetime <- max(accum_trafico_rodado$fecha_carga)
+
 labels_estado <- c("Fluido", "Denso", "Congestionado", "Cortado", "Sin datos",
                    str_c("Paso inferior ", c("fluido", "denso", "congestionado", "cortado")),
                    "Sin datos (paso inferior)")
@@ -20,25 +23,6 @@ accum_trafico_rodado <- read_csv("./data/accum_trafico.csv") %>%
     estado = factor(estado, levels = 0:9, labels = labels_estado),
     fecha_carga = clean_date_time(fecha_carga))
 
-first_datetime <- min(accum_trafico_rodado$fecha_carga)
-last_datetime <- max(accum_trafico_rodado$fecha_carga)
-
-traffic_changed <- accum_trafico_rodado %>% 
-  mutate(fecha_carga_next = fecha_carga + hours(1)) %>% 
-  left_join(accum_trafico_rodado, 
-            by = c("gid" = "gid",
-                   "fecha_carga_next" = "fecha_carga"),
-            suffix = c("", "_next"), keep = T) %>% 
-  filter(estado != estado_next, fecha_carga_next != last_datetime) %>% 
-  select(gid_next, estado_next, fecha_carga_next) %>% 
-  rename_with(~str_replace(.x, "_next", ""))
-
-traffic_first_last <- accum_trafico_rodado %>%
-  filter(fecha_carga == first_datetime | fecha_carga == last_datetime)
-
-# Calculamos los tramos que cambian de estado para no sobrecargar el renderizado del leaflet
-traffic_variation <- rbind(traffic_first_last, traffic_changed)
-
-trafico <- st_as_sf(left_join(traffic_variation, tramos_trafico, by = "gid") %>%
+trafico <- st_as_sf(left_join(accum_trafico_rodado, tramos_trafico, by = "gid") %>%
                       mutate(gid = as.character(gid)) %>% 
                       select(-denominacion, -idtramo))
